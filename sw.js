8485c26a4e341f312a8799b1b79447c2db35e75a
Thomas Loke <loekemeyer.n8n@gmail.com>
2026-04-18 22:50:10 -0300

    Stats ticket promedio: dolarized single value across all sales (v1.1)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'ventas-mv-v5';
+const CACHE = 'ventas-mv-v6';
 const CORE = ['./manifest.json', './icon-192.png', './icon-512.png', './logo.jpg'];
 
 self.addEventListener('install', (e) => {
